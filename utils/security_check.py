0f49aa0c9f3518d3bd9e85a4ba2b50b14a0f9976
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-03 13:29:49 +0100

    security_check.py

--- utils/security_check.py
+++ utils/security_check.py
@@ -1,1 +1,7 @@
+def check_token_security(token_info):
+    return (
+        token_info.get("sniffer_score", 0) >= 80 and
+        token_info.get("renounced") is True and
+        token_info.get("honeypot") is False
+    )
 
