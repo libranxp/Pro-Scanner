667fd59d344c774ae0cfa2f31827bb705d937843
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-03 15:59:20 +0100

    scan_crypto.py

--- scan/scan_crypto.py
+++ scan/scan_crypto.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fetch.fetch_crypto_data import fetch_crypto_data
 from fetch.fetch_sentiment_catalyst import fetch_sentiment_and_catalyst
-from dispatch.dispatch_telegram import send_telegram
+from dispatch.dispatch_telegram import send_telegram_message
 from dispatch.dispatch_discord import send_discord
 from dispatch.alert_formatter import format_alert
 from utils.scoring import score_crypto_alert
