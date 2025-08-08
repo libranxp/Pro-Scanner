e0fc3ed9e7b33a8fdbc8007399786abb90f18706
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-08 20:17:53 +0100

    logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,25 +1,6 @@
-import logging
-import os
-from datetime import datetime
+# utils/logger.py
+import datetime
 
-def init_logger(name="emeraldalert", log_dir="logs"):
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    timestamp = datetime.now().strftime("%Y-%m-%d")
-    log_path = os.path.join(log_dir, f"{name}_{timestamp}.log")
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    file_handler = logging.FileHandler(log_path)
-    stream_handler = logging.StreamHandler()
-
-    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-    file_handler.setFormatter(formatter)
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-
-    return logger
+def log(message):
+    timestamp = datetime.datetime.utcnow().isoformat()
+    print(f"[{timestamp}] {message}")
