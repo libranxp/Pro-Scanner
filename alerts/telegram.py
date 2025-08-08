2dc7b9405bbde884d3f585a9b0d1b816c2d51ca2
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-08 15:15:20 +0100

    telegram.py

--- alerts/telegram.py
+++ alerts/telegram.py
@@ -7,9 +7,9 @@ CHANNEL_IDS = {
     "stock": os.environ["TELEGRAM_STOCK_CHANNEL_ID"],
 }
 
-def send_telegram_alert(message, channel="crypto"):
+def send_telegram_alert(message, asset_type):
     token = os.environ["TELEGRAM_BOT_TOKEN"]
-    chat_id = CHANNEL_IDS[channel]
+    chat_id = CHANNEL_IDS.get(asset_type, os.environ["TELEGRAM_ADMIN_CHANNEL_ID"])
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
