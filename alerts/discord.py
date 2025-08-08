003ecb1bfefb9b37acfb1e62b6a0f426f08a4f18
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-08 15:15:50 +0100

    discord.py

--- alerts/discord.py
+++ alerts/discord.py
@@ -7,7 +7,8 @@ WEBHOOKS = {
     "stock": os.environ["DISCORD_STOCK_WEBHOOK"],
 }
 
-def send_discord_alert(message, channel="crypto"):
-    webhook_url = WEBHOOKS[channel]
+def send_discord_alert(message, asset_type):
+    webhook_url = WEBHOOKS.get(asset_type, os.environ["DISCORD_ADMIN_ERRORS_WEBHOOK"])
     payload = {"content": message}
     requests.post(webhook_url, json=payload)
+
